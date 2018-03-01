560a95b9da8355b841444fee4f33ea0e74e5b89d
LuanaCalili <luanacalili86@gmail.com>
2018-03-01 10:23:19 -0300

    Adicionado aplicação móvel para adivinhar números

---
+++ AdivinhaNumero/app/src/main/java/com/example/alunos/adivinhanumero/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alunos.adivinhanumero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Confirma() {
+        Scanner input = new Scanner(System.in);
+        Random gerador = new Random();
+        int x = gerador.nextInt(10) + 1;
+
+
+        int a;
+        int t;
+        t = 0;
+
+        while (t < 3) {
+            EditText userInput = findViewById(R.id.editText);
+            String teste = userInput.getText().toString();
+            a = Integer.parseInt(teste);
+
+            if (a == x) {
+                TextView etiqueta = (TextView) findViewById(R.id.Resultado);
+                etiqueta.setText(getResources().getString(R.string.lblResultado2));
+                break;
+            } else {
+                TextView etiqueta = (TextView) findViewById(R.id.Resultado);
+                etiqueta.setText(getResources().getString(R.string.lblResultado3));
+            }
+            t++;
+        }
+        TextView etiqueta = (TextView) findViewById(R.id.Resultado);
+        etiqueta.setText(getResources().getString(R.string.lblResultado4));
+
+    }
+}
